e7896eb4ff650089df5526f12e61745783752f50
Felix S. Klock II <pnkfelix@pnkfx.org>
2016-12-28 08:41:54 -0500

    Revise build script to only run tango on source files in `src/lit/src`.

--- tango-build.rs
+++ tango-build.rs
@@ -1,10 +1,26 @@
 extern crate tango;
 extern crate lalrpop;
 
+use std::env;
+
 fn main() {
     // I do not want lalrpop and tango to step on each others toes.
     // So we will segregate the two source trees.
 
-    // lalrpop::process_root().unwrap();
+    let cwd = env::current_dir().unwrap();
+    let grammar = {
+        let mut g = cwd.clone(); g.push("src/grammar"); g
+    };
+    let lit = {
+        let mut l = cwd.clone(); l.push("src/lit"); l
+    };
+    println!("grammar: {:?} lit: {:?}", grammar, lit);
+    env::set_current_dir(&grammar).unwrap();
+    env::set_current_dir(&lit).unwrap();
+
+    env::set_current_dir(grammar).unwrap();
+    lalrpop::process_root().unwrap();
+
+    env::set_current_dir(lit).unwrap();
     tango::process_root().unwrap();
 }
